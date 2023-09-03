e84193b9e8365bd49577e3361a9b6fd9038b7409
robot-ss <143159304+robot-ss@users.noreply.github.com>
2023-09-03 20:16:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,3 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
